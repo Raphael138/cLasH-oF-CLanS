7fd18eff3504bff1a30df06c384635720c05107c
Raphael Thesmar <thesmarraphael@gmail.com>
2023-06-21 15:24:31 -0400

    Model shit

--- model.py
+++ model.py
@@ -1,8 +1,7 @@
-import ultralytics
 from ultralytics import YOLO
 
 # Load a pretrained model 
 model = YOLO('yolov8n.pt')
 
 # Train and test model 
-results = model.train(data='coc.yaml', epochs=3)
+results = model.train(data='coc.yaml', epochs=10)
